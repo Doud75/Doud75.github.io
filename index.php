6ddb4c27e9183d719f03f083e4f3cb3f6c044924
Doud75 <adrienquimbre@gmail.com>
2022-03-10 21:39:03 +0100

    ajout du mastermind

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
             <span id="directionLeft" style="cursor: pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
             </span>
-            <a href="data.php" class="left">
+            <a href="data.php" class="left" target="_blank">
                 <img class="item" id="left" src="img/Mysql8.png">
             </a>
-            <a href="Rpg.php" class ="current" >
+            <a href="Rpg.php" class ="current" target="_blank" >
                 <img class="item" id="current" src="img/cmatrix-clear.png">
             </a>
-            <a href="ui-design.php" class ="right">
+            <a href="ui-design.php" class ="right" target="_blank">
                 <img class="item" id="right" src="img/unity-desktop-animation.webp">
             </a>
             <span id="directionRight" style="cursor: pointer">
--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function mymode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-let href = ["data.php", "Rpg.php", "ui-design.php"];
+let href = ["data.php", "Rpg.php", "ui-design.php", "mastermind/mastermind.html"];
 let href2 = [];
-let src = ["img/Mysql8.png", "img/cmatrix-clear.png", "img/unity-desktop-animation.webp"];
+let src = ["img/Mysql8.png", "img/cmatrix-clear.png", "img/unity-desktop-animation.webp", "img/81yJVdgJqML._AC_SL1500_.jpg"];
 let src2 = [];
 let left = document.querySelector('#directionLeft');
 let right = document.querySelector('#directionRight');
@@ -15,42 +15,49 @@ function right_function() {
     document.querySelector("#current").src = src[2];
     document.querySelector("#left").src = src[1];
     document.querySelector(".left").href = href[1];
-    document.querySelector("#right").src = src[0];
-    document.querySelector(".right").href = href[0];
+    document.querySelector("#right").src = src[3];
+    document.querySelector(".right").href = href[3];
     href2[0] = href[1];
     href2[1] = href[2];
-    href2[2] = href[0];
+    href2[2] = href[3];
+    href2[3] = href[0];
     href[0] = href2[0];
     href[1] = href2[1];
     href[2] = href2[2];
+    href[3] = href2[3];
     src2[0] = src[1];
     src2[1] = src[2];
-    src2[2] = src[0];
+    src2[2] = src[3];
+    src2[3] = src[0];
     src[0] = src2[0];
     src[1] = src2[1];
     src[2] = src2[2];
+    src[3] = src2[3];
 }
 
 function left_function() {
     document.querySelector(".current").href = href[0];
     document.querySelector("#current").src = src[0];
-    document.querySelector("#left").src = src[2];
-    document.querySelector(".left").href = href[2];
+    document.querySelector("#left").src = src[3];
+    document.querySelector(".left").href = href[3];
     document.querySelector("#right").src = src[1];
     document.querySelector(".right").href = href[1];
     href2[2] = href[1];
-    href2[0] = href[2];
+    href2[0] = href[3];
     href2[1] = href[0];
+    href2[3] = href[2]
     href[0] = href2[0];
     href[1] = href2[1];
     href[2] = href2[2];
+    href[3] = href2[3];
     src2[2] = src[1];
-    src2[0] = src[2];
+    src2[0] = src[3];
     src2[1] = src[0];
+    src2[3] = src[2];
     src[0] = src2[0];
     src[1] = src2[1];
     src[2] = src2[2];
-    console.log(document.querySelector(".current").href);
+    src[3] = src2[3];
 }
 
 function title_function() {
@@ -60,6 +67,8 @@ function title_function() {
         title.innerHTML = "RPG Python";
     } else if(document.querySelector(".current").href.includes("ui-design.php")) {
         title.innerHTML = "UI Design";
+    } else if(document.querySelector(".current").href.includes("mastermind")) {
+        title.innerHTML = "Mastermind";
     }
 }
 
